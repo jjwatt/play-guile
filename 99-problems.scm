5e860cd564c4a6be7e5d98559f13ae3fe0b21b86
Jesse Wattenbarger <jwatt@broken.watch>
2026-07-30 19:13:06 -0400

    More 99 problems

--- 99-problems.scm
+++ 99-problems.scm
@@ -126,4 +126,119 @@
         (_ acc))))
   (reverse (flatten lst)))
 
+;; Problem 08: Eliminate Duplicates.
+(define my-compress
+  (match-lambda
+    ;; Match a followed by a sub-list starting with b bound to t.
+    ((a . (and (b . _) t))
+     (if (equal? a b)
+         (my-compress t)
+         (cons a (my-compress t))))
+    (smaller smaller)))
 
+;; Alternative Problem 08: Eliminate Duplicates tail-recursive.
+(define (my-compress-tail lst)
+  (let loop ((acc '())
+             (l lst))
+    (match l
+      ;; Match adjacent duplicates and bind b . _ to t.
+      ((a . (and (b . _) t))
+       (if (equal? a b)
+           ;; Skip duplicate 'a'.
+           (loop acc t)
+           ;; Otherwise, keep 'a' and advance.
+           (loop (cons a acc) t)))
+      ((x) (reverse (cons x acc)))
+      ('() (reverse acc))
+      (_ #f))))
+
+;; Problem 09: Pack consecutive duplicates.
+(define (my-pack lst)
+  (let loop ((acc '())
+             (current '())
+             (l lst))
+    (match l
+      ((a . (and (b . _) t))
+       (if (equal? a b)
+           (loop acc (cons a current) t)
+           (loop (cons (cons a current) acc) '() t)))
+      ((x) (reverse (cons (cons x current) acc)))
+      (() '())
+      (_ #f))))
+
+;; Alternative Problem 09 with match-lambda*
+(define (my-pack* lst)
+  (define aux
+    (match-lambda*
+      ;; Emtpy input list.
+      ((acc current '())
+       '())
+      ;; Final element
+      ((acc current (x))
+       (reverse (cons (cons x current) acc)))
+      ;; Adjacent equal elements: add to current.
+      ((acc current (a . (and (b . _) t))) (=> next)
+       (if (equal? a b)
+           (aux acc (cons a current) t)
+           (next)))
+      ;; Adjacent non-equal elements: put current onto acc
+      ((acc current (a . (and (b . _) t)))
+       (aux (cons (cons a current) acc) '() t))))
+  (aux '() '() lst))
+;; (my-pack* '(a a a b c c d e e e e f))
+;; => ((a a a) (b) (c c) (d) (e e e e) (f))
+
+;; Alternative Problem 10: RLE using Problem 9 solution
+(define (my-rle-via-pack lst)
+  (map (match-lambda
+         ((and (x . _) sublist)
+          (cons (length sublist) x)))
+       (my-pack* lst)))
+;; (my-rle-via-pack '(a a a b c c d))
+;; =>  ((3 . a) (1 . b) (2 . c) (1 . d))
+
+
+;; Problem 10: RLE
+(define (my-rle lst)
+  (let loop ((count 0)
+             (acc '())
+             (l lst))
+    (match l
+      ((a . (and (b . _) t))
+       (if (equal? a b)
+           (loop (+ count 1) acc t)
+           (loop 0 (cons (cons (+ count 1) a) acc) t)))
+      ((x) (reverse (cons (cons (+ count 1) x) acc)))
+      (() '())
+      (_ #f))))
+;; (my-rle '(a a a b c c d))
+;; => ((3 . a) (1 . b) (2 . c) (1 . d))
+
+;; Problem 11: Modified RLE
+(define (my-modified-rle lst)
+  (let loop ((count 0)
+             (acc '())
+             (l lst))
+    (match l
+      ((a . (and (b . _) t))
+       (if (equal? a b)
+           (loop (+ count 1) acc t)
+           (let ((entry (if (= count 0) a (cons (+ count 1) a))))
+             (loop 0 (cons entry acc) t))))
+      ((x)
+       (let ((entry (if (= count 0) x (cons (+ count 1) x))))
+         (reverse (cons entry acc))))
+      (() '())
+      (_ #f))))
+;; (my-modified-rle '(a a a b c c d))
+;; => ((3 . a) b (2 . c) d)
+
+;; Alternative Prolbem 11: Modified RLE via my-pack*
+(define (my-modified-rle-via-pack lst)
+  (map (match-lambda
+         ((x) x)
+         ((and (x . _) sublist)
+          (cons (length sublist) x)))
+       (my-pack* lst)))
+;; (my-modified-rle-via-pack '(a a a b c c d))
+;; => ((3 . a) b (2 . c) d)
