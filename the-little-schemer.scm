324ba942ad9a844445e06996244f8a17c4c0396a
Jesse Wattenbarger <jwatt@broken.watch>
2026-08-03 12:58:55 -0400

    Fixed and added more matching on little-schemer

--- the-little-schemer.scm
+++ the-little-schemer.scm
@@ -10,14 +10,38 @@
      (lat? tail))
     (_ #f)))
 
-(define member?
+(define (member? a lat)
+  (match lat
+    (() #f)
+    ((head . tail)
+     (if (eq? head a) #t
+         (member? a tail)))))
+
+(define member-match-lambda*?
   (match-lambda*
-    (_ () #f)
-    (a ((? (lambda (x) (eq? x a))) . _) #t)
-    (a (_ . tail) (member? a tail))))
+    ((_ ()) #f)
+    ((a ((? (lambda (x) (eq? a x)) head) . tail)) #t)
+    ((a (_ . tail))
+     (member-match-lambda*? a tail))))
+
+(define (rember? a lat)
+  (match lat
+    (() '())
+    ((head . tail)
+     (if (eq? head a)
+         tail
+         (cons head (rember? a tail))))))
+
+(define rember-match-lambda*?
+  (match-lambda*
+    ((_ ()) '())
+    ((a ((? (lambda (x) (eq? a x)) head) . tail)) tail)
+    ((a (head . tail))
+     (cons head (rember-match-lambda*? a tail)))))
 
 (define firsts
   (match-lambda
     (() '())
     (((first-elem . _) tail)
      (cons first-elem (firsts tail)))))
+
